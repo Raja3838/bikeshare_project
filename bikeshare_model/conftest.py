607832635d666d063098fcba3dab3d3c6ba21a79
Raja Kotipalli <66884043+Raja3838@users.noreply.github.com>
2023-06-30 00:33:45 +0200

    Update conftest.py

--- bikeshare_model/conftest.py
+++ bikeshare_model/conftest.py
@@ -1,3 +1,6 @@
+73% of storage used … 
+You use storage when you save to Drive, back up to Google Photos and send and receive email on Gmail
+
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
@@ -12,3 +15,14 @@ from bikeshare_model.processing.data_manager import load_dataset
 @pytest.fixture
 def sample_input_data():
     data = load_dataset(file_name = config.app_config.training_data_file)
+
+    # divide train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        
+        data[config.model_config.features],     # predictors
+        data[config.model_config.target],       # target
+        test_size = config.model_config.test_size,
+        random_state=config.model_config.random_state,   # set the random seed here for reproducibility
+    )
+
+    return X_test, y_test
