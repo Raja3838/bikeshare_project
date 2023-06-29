9766bf7f9e734b467b84c6017eea0f4b2f2ff684
Raja Kotipalli <66884043+Raja3838@users.noreply.github.com>
2023-06-29 23:24:56 +0200

    Update conftest.py

--- bikeshare_model/conftest.py
+++ bikeshare_model/conftest.py
@@ -12,14 +12,3 @@ from bikeshare_model.processing.data_manager import load_dataset
 @pytest.fixture
 def sample_input_data():
     data = load_dataset(file_name = config.app_config.training_data_file)
-
-    # divide train and test
-    X_train, X_test, y_train, y_test = train_test_split(
-        
-        data[config.model_config.features],     # predictors
-        data[config.model_config.target],       # target
-        test_size = config.model_config.test_size,
-        random_state=config.model_config.random_state,   # set the random seed here for reproducibility
-    )
-
-    return X_test, y_test
